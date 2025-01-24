4e333cc322b9a1764f0d4244b4f8389eafc31222
ThelmaPana <thelma@crans.org>
2025-01-24 16:58:18 +0000

    Remove inland points for glob obs

--- utils.R
+++ utils.R
@@ -16,5 +16,8 @@ theme_set(theme_minimal())
 # Get world map data
 world <- fortify(map_data('world', wrap = c(-180, 180))) %>% rename(lon = long)
 
+# To compute inland points
+coast <- read_csv("data/raw/gshhg_world_c.csv", col_types = cols())
+
 # Earth radius
 R <- 6378.137 # km # https://nssdc.gsfc.nasa.gov/planetary/factsheet/earthfact.html
